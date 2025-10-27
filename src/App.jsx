e95f2dce8ae77775080f7e50ae29d0eeb7e7b9e7
Gracogen <gracogenolu06@gmail.com>
2025-10-27 12:25:05 +0100

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ContactCard from "./components/ContactCard";
 import AddAndUpdateContact from "./components/AddAndUpdateContact";
 import useDisclouse from "./hooks/useDisclouse";
 import NotFoundContact from "./components/NotFoundContact";
+
 const App = () => {
   const [contacts, setContacts] = useState([]);
 
